b738368164778316cef02bdca704c5f6d24b62e8
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-10-18 11:18:43 +0300

    feat: add ng-class & styles to table data

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
-import angular from 'angular';
-import './style.scss';
+import angular from "angular";
+import "./style.scss";
 
 const app = angular.module("phonebookApp", []);
 
 app.controller("MainController", function searchCtrl($scope, $http) {
-  $http.get("data.json").then(res => ($scope.data = res.data));
+  $http.get("data.json").then(res => {
+    $scope.data = res.data;
+
+    $scope.highlite = firstLetter => {
+      switch (firstLetter.toLowerCase()) {
+        case "a":
+          return "red_field";
+        case "b":
+          return "blue_field";
+        default:
+          return "";
+      }
+    };
+  });
 });
