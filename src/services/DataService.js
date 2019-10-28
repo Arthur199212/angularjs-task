ee671850909b598b599fbd387ab24824e622a8b3
Artur Rubchenko <Artur_Rubchenko@epam.com>
2019-10-28 17:02:17 +0300

    feat: all contacts added to localStorage

--- src/services/DataService.js
+++ src/services/DataService.js
@@ -1,31 +1,57 @@
 import idGenerator from "../helpers/idGenerator";
 
 export default function DataService($http) {
-    this.getAll = () => {
-      return $http.get("https://raw.githubusercontent.com/Arthur199212/angularjs-task/master/src/data/db.json")
-        .then(res => {
-          this.data = res.data;
-          return res.data;
-        })
-        .catch("Faild to load data");
-    };
+  this.saveDataToLocalStorage = () =>
+    localStorage.setItem("savedData", JSON.stringify(this.data));
 
-    this.addContact = (name, phone) => {
-      if (name && phone) {
-        const newContact = {
-          id: idGenerator(),
-          name: name,
-          phone: phone
-        };
+  this.getAll = () => {
+    return $http
+      .get(
+        "https://raw.githubusercontent.com/Arthur199212/angularjs-task/master/src/data/db.json"
+      )
+      .then(res => {
+        this.data = res.data;
 
-        this.data = [...this.data, newContact];
-      }
+        return res.data;
+      })
+      .catch("Faild to load data");
+  };
 
-      return this.data;
-    };
+  this.getSavedData = () =>
+    this.data = JSON.parse(localStorage.getItem("savedData"));
 
-    this.removeContact = id => {
-      this.data = this.data.filter(item => item.id !== id);
-      return this.data;
-    };
+  this.addContact = (name, phone) => {
+    if (name && phone) {
+      const newContact = {
+        id: idGenerator(),
+        name: name,
+        phone: phone
+      };
+
+      this.data = [...this.data, newContact];
+
+      this.saveDataToLocalStorage();
+    }
+
+    return this.data;
+  };
+
+  this.editContact = editedContact => {
+    this.data = [
+      ...this.data.filter(({ id }) => editedContact.id !== id),
+      editedContact
+    ];
+
+    this.saveDataToLocalStorage();
+
+    return this.data;
+  };
+
+  this.removeContact = id => {
+    this.data = this.data.filter(item => item.id !== id);
+
+    this.saveDataToLocalStorage();
+
+    return this.data;
+  };
 }
